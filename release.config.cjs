dc9e7f51302bd86f934332ac43d175943a1ceb36
Gavin Mogan <git@gavinmogan.com>
2023-03-27 16:14:03 -0700

    feat: yaml quote the commits

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,11 @@
+const yaml = require('js-yaml');
+const Handlebars = require('handlebars')
+
+Handlebars.registerHelper("yamlString", function (options) {
+  const input = options.fn(this).trim()
+  return yaml.dump(input.replace(/^\s*\*\s*/g, '').trim(), {lineWith: 999, forceQuotes: true});
+});
+
 module.exports = {
   "branches": [
     "main",
@@ -9,58 +17,7 @@ module.exports = {
       "@semantic-release/release-notes-generator",
       {
         "writerOpts": {
-          "mainTemplate": "{{#each commitGroups}}{{#each commits}}{{> commit root=@root}}\n{{/each}}{{/each}}",
-          "commitPartial": `- {{header}}: {{~!-- commit link --}}
-{{~#if @root.linkReferences}} ([{{hash}}](
-  {{~#if @root.repository}}
-    {{~#if @root.host}}
-      {{~@root.host}}/
-    {{~/if}}
-    {{~#if @root.owner}}
-      {{~@root.owner}}/
-    {{~/if}}
-    {{~@root.repository}}
-  {{~else}}
-    {{~@root.repoUrl}}
-  {{~/if}}/
-  {{~@root.commit}}/{{hash}}))
-{{~else if hash}} {{hash}}{{~/if}}
-
-{{~!-- commit references --}}
-{{~#if references~}}
-  , closes
-  {{~#each references}} {{#if @root.linkReferences~}}
-    [
-    {{~#if this.owner}}
-      {{~this.owner}}/
-    {{~/if}}
-    {{~this.repository}}#{{this.issue}}](
-    {{~#if @root.repository}}
-      {{~#if @root.host}}
-        {{~@root.host}}/
-      {{~/if}}
-      {{~#if this.repository}}
-        {{~#if this.owner}}
-          {{~this.owner}}/
-        {{~/if}}
-        {{~this.repository}}
-      {{~else}}
-        {{~#if @root.owner}}
-          {{~@root.owner}}/
-        {{~/if}}
-          {{~@root.repository}}
-        {{~/if}}
-    {{~else}}
-      {{~@root.repoUrl}}
-    {{~/if}}/
-    {{~@root.issue}}/{{this.issue}})
-  {{~else}}
-    {{~#if this.owner}}
-      {{~this.owner}}/
-    {{~/if}}
-    {{~this.repository}}#{{this.issue}}
-  {{~/if}}{{/each}}
-{{~/if}}`
+          "mainTemplate": "{{#each commitGroups}}{{#each commits}} - {{#yamlString}}{{> commit root=@root}}{{/yamlString}}{{/each}}{{/each}}",
         }
       }
     ],
